f425519422e4fc01e2b97e016a0a084d3ebd97d6
hwjoo <savior0826@gmail.com>
2017-01-11 18:11:22 +0900

    sycretware lib added

--- app/src/main/java/com/muhanbit/sycrethealth/LoginActivity.java
+++ app/src/main/java/com/muhanbit/sycrethealth/LoginActivity.java
@@ -2,11 +2,15 @@ package com.muhanbit.sycrethealth;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
 import com.muhanbit.sycrethealth.view.LoginView;
+import com.sycretware.auth.Environment;
+import com.sycretware.auth.KeyStore;
+import com.sycretware.auth.Provider;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -28,10 +32,19 @@ public class LoginActivity extends AppCompatActivity implements LoginView {
         ButterKnife.bind(this);
 
 
+
     }
 
     @OnClick(R.id.login_btn) void loginBtnClick(){
-        Toast.makeText(LoginActivity.this, "click", Toast.LENGTH_SHORT).show();
+        if(Environment.initialize(this)) {
+            Provider mProvider = new Provider();
+            if(mProvider.init("123456")){
+                Toast.makeText(LoginActivity.this,"login success", Toast.LENGTH_SHORT).show();
+            }else{
+                Toast.makeText(LoginActivity.this,"login fail", Toast.LENGTH_SHORT).show();
+            }
+
+        }
     }
 
 }
